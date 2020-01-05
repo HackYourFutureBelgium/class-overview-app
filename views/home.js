718107ae9aa3f3bc741ff194d1a456e924e55fef
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-01-05 20:26:31 +0100

    stateful-bug

--- views/home.js
+++ views/home.js
@@ -2,17 +2,11 @@ import displayMany from './display-many.js'
 import moduleThumb from './module-thumb.js'
 import studentThumb from './student-thumb.js'
 
-let rendered = null;
-
 export default async state => {
 
   try {
     history.pushState({}, null, window.location.pathname);
   } catch (err) { };
-
-  if (rendered !== null) {
-    return rendered;
-  }
 
   const container = document.createElement('div');
   container.id = state.repoName;
@@ -20,8 +14,6 @@ export default async state => {
   container.appendChild(await displayMany(state, state.modules, moduleThumb, 'Modules'));
   container.appendChild(document.createElement('hr'));
   container.appendChild(await displayMany(state, state.students, studentThumb, 'Students'));
-
-  rendered = container;
 
   return container;
 

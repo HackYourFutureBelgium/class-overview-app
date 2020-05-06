2c60d95a33e2f810e652e17d294c2fb6ba0fac62
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-05-06 22:18:18 +0200

    better-header-footer

--- src/views/header-footer.js
+++ src/views/header-footer.js
@@ -1,14 +1,13 @@
-import home from "./home.js";
-import randomizer from "./randomizer.js"
+import home from './home.js';
+import randomizer from './randomizer.js';
 
-export default (state) => {
-
+export default state => {
   const container = document.createElement('div');
   container.className = 'header-footer';
   container.id = 'class-app';
 
   const title = document.createElement('h1');
-  title.innerHTML = state.repoName
+  title.innerHTML = state.repoName;
   title.className = 'header-footer-title';
   container.appendChild(title);
 
@@ -23,43 +22,35 @@ export default (state) => {
     state.currentStudent = null;
     state.body.innerHTML = '';
     state.body.appendChild(await home(state));
-  }
+  };
   overviewButton.onclick = toHome;
   header.appendChild(overviewButton);
 
-  const repoA = document.createElement('a');
-  repoA.href = "https://github.com/" + state.userName + "/" + state.repoName;
-  repoA.target = "_blank";
-  const repoButton = document.createElement('button');
-  repoButton.innerHTML = 'Class Repository'
-  repoA.appendChild(repoButton);
-  header.appendChild(repoA);
+  const boardsA = document.createElement('a');
+  boardsA.href =
+    'https://github.com/' + state.userName + '/' + state.repoName + '/projects';
+  boardsA.target = '_blank';
+  const boardsButton = document.createElement('button');
+  boardsButton.innerHTML = 'Class HW Boards';
+  boardsA.appendChild(boardsButton);
+  header.appendChild(boardsA);
 
   const issuesA = document.createElement('a');
-  issuesA.href = "https://github.com/" + state.userName + "/" + state.repoName + '/issues';
-  issuesA.target = "_blank";
+  issuesA.href =
+    'https://github.com/' + state.userName + '/' + state.repoName + '/issues';
+  issuesA.target = '_blank';
   const issuesButton = document.createElement('button');
-  issuesButton.innerHTML = 'Class Issues'
+  issuesButton.innerHTML = 'Class Issues';
   issuesA.appendChild(issuesButton);
   header.appendChild(issuesA);
-  header.appendChild(document.createElement('br'));
 
-  const homeworkSubmissionA = document.createElement('a');
-  homeworkSubmissionA.href = "https://home.hackyourfuture.be/students/homework-submission";
-  homeworkSubmissionA.target = "_blank";
-  const homeworkSubmissionButton = document.createElement('button');
-  homeworkSubmissionButton.innerHTML = 'Homework Submission'
-  homeworkSubmissionA.appendChild(homeworkSubmissionButton);
-  header.appendChild(homeworkSubmissionA);
-
-  const studyResourcesA = document.createElement('a');
-  studyResourcesA.href = "https://study.hackyourfuture.be";
-  studyResourcesA.target = "_blank";
-  const studyResourcesButton = document.createElement('button');
-  studyResourcesButton.innerHTML = 'Study Links';
-  studyResourcesA.appendChild(studyResourcesButton);
-  header.appendChild(studyResourcesA);
-
+  const repoA = document.createElement('a');
+  repoA.href = 'https://github.com/' + state.userName + '/' + state.repoName;
+  repoA.target = '_blank';
+  const repoButton = document.createElement('button');
+  repoButton.innerHTML = 'Class Repository';
+  repoA.appendChild(repoButton);
+  header.appendChild(repoA);
 
   const randomizerButton = document.createElement('button');
   randomizerButton.innerHTML = 'class randomizer';
@@ -70,6 +61,42 @@ export default (state) => {
     state.body.appendChild(await randomizer(state));
   };
   header.appendChild(randomizerButton);
+
+  header.appendChild(document.createElement('br'));
+  header.appendChild(document.createElement('br'));
+
+  const homeworkSubmissionA = document.createElement('a');
+  homeworkSubmissionA.href =
+    'https://home.hackyourfuture.be/students/homework-submission';
+  homeworkSubmissionA.target = '_blank';
+  const homeworkSubmissionButton = document.createElement('button');
+  homeworkSubmissionButton.innerHTML = 'Homework Submission';
+  homeworkSubmissionA.appendChild(homeworkSubmissionButton);
+  header.appendChild(homeworkSubmissionA);
+
+  const studyResourcesA = document.createElement('a');
+  studyResourcesA.href = 'https://study.hackyourfuture.be';
+  studyResourcesA.target = '_blank';
+  const studyResourcesButton = document.createElement('button');
+  studyResourcesButton.innerHTML = 'Study Links';
+  studyResourcesA.appendChild(studyResourcesButton);
+  header.appendChild(studyResourcesA);
+
+  const curriculumA = document.createElement('a');
+  curriculumA.href = 'https://home.hackyourfuture.be/curriculum';
+  curriculumA.target = '_blank';
+  const curriculumButton = document.createElement('button');
+  curriculumButton.innerHTML = 'Curriculum';
+  curriculumA.appendChild(curriculumButton);
+  header.appendChild(curriculumA);
+
+  const studentGuidebookA = document.createElement('a');
+  studentGuidebookA.href = 'https://home.hackyourfuture.be/students';
+  studentGuidebookA.target = '_blank';
+  const studentGuidebookButton = document.createElement('button');
+  studentGuidebookButton.innerHTML = 'Student Guidebook';
+  studentGuidebookA.appendChild(studentGuidebookButton);
+  header.appendChild(studentGuidebookA);
 
   container.appendChild(header);
 
@@ -82,21 +109,23 @@ export default (state) => {
   container.appendChild(document.createElement('hr'));
   container.appendChild(document.createElement('hr'));
 
-  const footer = document.createElement('div');
-  footer.className = 'header-footer-footer';
+  const footer = header.cloneNode(true);
+  footer.childNodes[0].onclick = toHome;
 
-  const bottomOverviewButton = overviewButton.cloneNode(true);
-  bottomOverviewButton.onclick = toHome;
-  footer.appendChild(bottomOverviewButton);
+  // const footer = document.createElement('div');
+  // footer.className = 'header-footer-footer';
 
-  footer.appendChild(repoA.cloneNode(true));
-  footer.appendChild(issuesA.cloneNode(true));
-  footer.appendChild(document.createElement('br'));
-  footer.appendChild(homeworkSubmissionA.cloneNode(true));
-  footer.appendChild(studyResourcesA.cloneNode(true));
-  footer.appendChild(randomizerButton.cloneNode(true));
+  // const bottomOverviewButton = overviewButton.cloneNode(true);
+  // bottomOverviewButton.onclick = toHome;
+  // footer.appendChild(bottomOverviewButton);
+
+  // footer.appendChild(repoA.cloneNode(true));
+  // footer.appendChild(issuesA.cloneNode(true));
+  // footer.appendChild(document.createElement('br'));
+  // footer.appendChild(homeworkSubmissionA.cloneNode(true));
+  // footer.appendChild(studyResourcesA.cloneNode(true));
+  // footer.appendChild(randomizerButton.cloneNode(true));
   container.appendChild(footer);
 
   return container;
-
-}
+};
